3a0c5122d34ac0412f65e10767ad22538b6a3a07
Osame-Yu <93514585+Yusuke-Uchinou@users.noreply.github.com>
2021-12-20 16:17:41 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password,
-    format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'must include both numbers and letters' }
+            format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'must include both numbers and letters' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,62 +7,62 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nickname、email、password、password_confirmationがあれば登録できる" do
+      it 'nickname、email、password、password_confirmationがあれば登録できる' do
         expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
-      it "nicknameがなければ登録できない" do
+      it 'nicknameがなければ登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      it "emailがなければ登録できない" do
+      it 'emailがなければ登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      it "emailには@が含まれていなければ登録できない" do
+      it 'emailには@が含まれていなければ登録できない' do
         @user.email = 'aaaaaaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
-      it "重複したemailが存在する場合は登録できない" do
+      it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
-      it "passwordがなければ登録できない" do
+      it 'passwordがなければ登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "passwordとpassword_confirmationが同一でなければ登録できない" do
+      it 'passwordとpassword_confirmationが同一でなければ登録できない' do
         @user.password = 'a12345'
         @user.password = 'a123456'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it "passwordが数字のみでは登録できない" do
+      it 'passwordが数字のみでは登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password must include both numbers and letters')
       end
-      it "passwordが英字のみでは登録できない" do
+      it 'passwordが英字のみでは登録できない' do
         @user.password = 'abcdefg'
         @user.password_confirmation = 'abcdefg'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password must include both numbers and letters')
       end
-      it "passwordに半角英数字以外が含まれていると登録できない" do
+      it 'passwordに半角英数字以外が含まれていると登録できない' do
         @user.password = 'あ23456'
         @user.password_confirmation = 'あ23456'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password must include both numbers and letters')
       end
-      it "passwordが5文字以下では登録できない" do
+      it 'passwordが5文字以下では登録できない' do
         @user.password = 'a1234'
         @user.password_confirmation = 'a1234'
         @user.valid?
