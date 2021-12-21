e442dd37524194b8c2b52a5f0b73119b4b85d6cb
Osame-Yu <93514585+Yusuke-Uchinou@users.noreply.github.com>
2021-12-22 02:05:24 +0900

    creator単体テストコードに追記

--- spec/factories/creators.rb
+++ spec/factories/creators.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :creator do
     creator_name { Faker::Name.first_name }
+    creator_short_text { Faker::Lorem.sentence(word_count: 6) }
     creator_text { Faker::Lorem.sentence }
   end
 end
